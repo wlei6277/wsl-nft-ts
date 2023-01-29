5371ebc6871bbfef8391782866d9cffe36084637
Billy Leitch <billy.leitch@tshirtventures.com.au>
2023-01-29 19:41:42 +1100

    Deployed contract wooohooo

--- packages/hardhat-ts/deploy/01.WSLNFT.deploy.ts
+++ packages/hardhat-ts/deploy/01.WSLNFT.deploy.ts
@@ -2,13 +2,13 @@ import { DeployFunction } from 'hardhat-deploy/types';
 import { HardhatRuntimeEnvironmentExtended } from 'helpers/types/hardhat-type-extensions';
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironmentExtended) => {
-  const { getNamedAccounts, deployments } = hre;
+  const { getNamedAccounts, deployments, ethers } = hre;
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
+
   await deploy('WSLNFT', {
     // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
     from: deployer,
-    // args: ["Hello"],
     log: true,
   });
 
