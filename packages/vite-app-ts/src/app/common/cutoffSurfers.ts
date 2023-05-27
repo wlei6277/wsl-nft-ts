3f2fc171bd82fea2b0b820d5b07c9b1f9080f68e
Billy Leitch <billy.leitch@tshirtventures.com.au>
2023-05-27 11:01:36 +1000

    feat: added UI to show cutoff round

---
+++ packages/vite-app-ts/src/app/common/cutoffSurfers.ts
@@ -0,0 +1,1 @@
+export const remainingSurferTokenIds = [16, 12, 7, 5, 9, 32, 17, 35, 22, 2, 4, 3, 8, 18, 24, 11, 23, 31, 30, 19, 1, 34, 10];
--- packages/vite-app-ts/src/app/routes/main/components/Surfers.tsx
+++ packages/vite-app-ts/src/app/routes/main/components/Surfers.tsx
@@ -1,7 +1,8 @@
-import { Card, Spin, Button, Space, Avatar } from 'antd';
+import { Card, Spin, Button, Space, Avatar, Badge } from 'antd';
 import { useEthersContext } from 'eth-hooks/context';
 import { useScaffoldProviders as useScaffoldAppProviders } from '~~/app/routes/main/hooks/useScaffoldAppProviders';
 import { FC } from 'react';
+import { remainingSurferTokenIds } from '../../../common/cutoffSurfers';
 
 import { SurferData } from '../Main';
 
@@ -19,9 +20,10 @@ interface SurferProps {
   initialPriceUsd: string;
   buySurfer: (tokenId: string) => Promise<void>;
   nftContractAddress: string;
+  isDisabled: boolean;
 }
 
-const Surfer: FC<SurferProps> = ({ surfer, buySurfer, initialPriceUsd, nftContractAddress }) => {
+const Surfer: FC<SurferProps> = ({ surfer, buySurfer, initialPriceUsd, nftContractAddress, isDisabled }) => {
   const { account } = useEthersContext();
   const { targetNetwork } = useScaffoldAppProviders();
   const { name, tokenId, imgUrl, isAvailable, ownerAddress, owner } = surfer;
@@ -52,6 +54,7 @@ const Surfer: FC<SurferProps> = ({ surfer, buySurfer, initialPriceUsd, nftContra
           type="primary"
           className="mt-4"
           block
+          disabled={isDisabled}
           onClick={(): void => {
             buySurfer(tokenId).catch((error) => console.error(error));
           }}>
@@ -59,7 +62,7 @@ const Surfer: FC<SurferProps> = ({ surfer, buySurfer, initialPriceUsd, nftContra
         </Button>
       )}
       {!isAvailable && account && !isOwner && openseaLink && (
-        <Button type="link" className="mt-4 w-full" href={openseaLink}>
+        <Button type="link" className="mt-4 w-full" href={openseaLink} disabled={isDisabled}>
           Make Offer
         </Button>
       )}
@@ -71,18 +74,34 @@ const Surfers: FC<SurfersProps> = ({ surferData, initialPriceUsd, buySurfer, nft
   return (
     <Space wrap size="middle" align="center" className="justify-center">
       {surferData[0] ? (
-        surferData.map(
-          (surfer) =>
-            surfer.tokenId !== '31' && (
+        surferData.map((surfer) => {
+          const madeCutoffRound = remainingSurferTokenIds.includes(Number(surfer.tokenId));
+          const isDuplicateRioNFT = surfer.tokenId === '31';
+          if (isDuplicateRioNFT) return;
+          if (madeCutoffRound)
+            return (
               <Surfer
                 surfer={surfer}
                 key={`nft-${surfer.tokenId}`}
                 initialPriceUsd={initialPriceUsd}
                 buySurfer={buySurfer}
                 nftContractAddress={nftContractAddress}
+                isDisabled={false}
               />
-            )
-        )
+            );
+          return (
+            <Badge.Ribbon text="Eliminated" color="grey">
+              <Surfer
+                surfer={surfer}
+                key={`nft-${surfer.tokenId}`}
+                initialPriceUsd={initialPriceUsd}
+                buySurfer={buySurfer}
+                nftContractAddress={nftContractAddress}
+                isDisabled
+              />
+            </Badge.Ribbon>
+          );
+        })
       ) : (
         <Spin tip="Loading surfers...." size="large" />
       )}
